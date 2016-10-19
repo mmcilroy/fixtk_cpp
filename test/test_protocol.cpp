f418c4d9c4fd43b061b17de034491600dde4fc35
mmcilroy <m1rk.mci@gmail.com>
2016-10-19 23:23:08 +0100

    refactor application class

--- test/test_protocol.cpp
+++ test/test_protocol.cpp
@@ -1,10 +1,17 @@
 
 #include "session_factory.hpp"
 
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
 namespace fix {
 
-class protocol : public session::receiver {
+class application : public session::receiver {
 public:
+    bool is_logged_on() const {
+        return logged_on_;
+    }
+
     void receive( session& sess, const message& msg ) override {
         auto seq_received = std::stoi( find_field( 34, msg ) );
         auto seq_expected = sess.get_receive_sequence();
@@ -50,7 +57,19 @@ protected:
     virtual void process( session& sess, const message& msg, const message_type& type, const sequence& seq_received ) {
         log_debug( "processing message " << seq_received );
         sess.set_receive_sequence( 1 + seq_received );
-        // process message here!
+        if( type == "2" ) {
+            auto low = find_field( 7, msg );
+            auto high = find_field( 16, msg );
+            for( int i = std::stoi( low ); i <= std::stoi( high ); i++ ) {
+                auto resend_msg = sess.get_sent( i );
+                sess.send( find_field( 35, resend_msg ), resend_msg );
+            }
+        } else if( type == "A" ) {
+            // if initiator now we're logged on
+            // if acceptor nothing required
+        } else {
+            // process application message here!        
+        }
 
         // handle any queued messages that are next in sequence
         while( queue_.size() && queue_.front().first == sess.get_receive_sequence() ) {
@@ -100,19 +119,92 @@ private:
     std::list< std::pair< sequence, message > > queue_;
 };
 
-struct alloc_protocol {
+struct alloc_application {
     session::receiver* operator()() {
-        return new protocol;
+        return new application;
     }
 };
 
 }
 
-int main() {
-    fix::session_factory_impl< fix::alloc_protocol > factory;
+TEST_CASE( "application", "[]" ) {
+    fix::session_factory_impl< fix::alloc_application > factory;
     fix::session* sess = factory.get_session( { "P", "T", "S" } );
-    sess->receive( fix::parse( "8=P|9=??|35=A|34=3|49=S|56=T|10=??|" ) );
-    sess->receive( fix::parse( "8=P|9=??|35=A|34=4|49=S|56=T|10=??|" ) );
-    sess->receive( fix::parse( "8=P|9=??|35=D|34=1|49=S|56=T|10=??|" ) );
-    sess->receive( fix::parse( "8=P|9=??|35=D|34=2|49=S|56=T|10=??|" ) );
+    fix::application* appl = (fix::application*)sess->get_receiver();
+
+    REQUIRE( appl != nullptr );
+    REQUIRE( appl->is_logged_on() == false );
+
+    SECTION( "successful logon, sequence expected", "[]" ) {
+        sess->receive( fix::parse( "8=P|9=??|35=A|34=1|49=S|56=T|10=??|" ) );
+        REQUIRE( fix::parse( "8=P|9=??|35=A|34=1|49=T|56=S|10=??|" ) == sess->get_sent( 1 ) );
+        REQUIRE( appl->is_logged_on() );
+    }
+
+    SECTION( "successful logon, sequence too high", "[]" ) {
+        sess->receive( fix::parse( "8=P|9=??|35=A|34=666|49=S|56=T|10=??|" ) );
+        REQUIRE( fix::parse( "8=P|9=??|35=A|34=1|49=T|56=S|10=??|" ) == sess->get_sent( 1 ) );
+        REQUIRE( fix::parse( "8=P|9=??|35=2|34=2|49=T|56=S|7=1|16=665|10=??|" ) == sess->get_sent( 2 ) );
+        REQUIRE( appl->is_logged_on() );
+    }
+
+    SECTION( "unsuccessful logon, sequence too low", "[]" ) {
+        sess->set_receive_sequence( 666 );
+        sess->receive( fix::parse( "8=P|9=??|35=A|34=1|49=S|56=T|10=??|" ) );
+        REQUIRE( appl->is_logged_on() == false);
+    }
+
+    SECTION( "unsuccessful logon, first message not a logon", "[]" ) {
+        sess->receive( fix::parse( "8=P|9=??|35=X|34=1|49=S|56=T|10=??|" ) );
+        REQUIRE( appl->is_logged_on() == false);
+    }
+
+    SECTION( "successful logon, gap fill 1-3 ok", "[]" ) {
+        sess->receive( fix::parse( "8=P|9=??|35=A|34=4|49=S|56=T|10=??|" ) );
+        REQUIRE( fix::parse( "8=P|9=??|35=A|34=1|49=T|56=S|10=??|" ) == sess->get_sent( 1 ) );
+        REQUIRE( fix::parse( "8=P|9=??|35=2|34=2|49=T|56=S|7=1|16=3|10=??|" ) == sess->get_sent( 2 ) );
+        REQUIRE( appl->is_logged_on() );
+        REQUIRE( sess->get_receive_sequence() == 1 );
+        sess->receive( fix::parse( "8=P|9=??|35=A|34=1|49=S|56=T|10=??|" ) );
+        REQUIRE( sess->get_receive_sequence() == 2 );
+        sess->receive( fix::parse( "8=P|9=??|35=D|34=2|49=S|56=T|10=??|" ) );
+        REQUIRE( sess->get_receive_sequence() == 3 );
+        sess->receive( fix::parse( "8=P|9=??|35=D|34=3|49=S|56=T|10=??|" ) );
+        REQUIRE( sess->get_receive_sequence() == 5 );
+        REQUIRE( appl->is_logged_on() );
+    }
+
+    /*
+    SECTION( "successful logon, gap fill 1-3, missing 1" ) {
+        sess->receive( fix::parse( "8=P|9=??|35=A|34=4|49=S|56=T|10=??|" ) );
+        REQUIRE( sender->sent( "8=P|9=??|35=A|34=1|49=T|56=S|10=??|" ) );
+        REQUIRE( sender->sent( "8=P|9=??|35=2|34=2|49=T|56=S|7=1|16=3|10=??|" ) );
+        REQUIRE( appl->is_logged_on() );
+        sess->receive( fix::parse( "8=P|9=??|35=D|34=2|49=S|56=T|10=??|" ) );
+        sess->receive( fix::parse( "8=P|9=??|35=D|34=3|49=S|56=T|10=??|" ) );
+        REQUIRE( !appl->is_logged_on() );
+        REQUIRE( !sess->is_connected() );
+    }
+
+    SECTION( "successful logon, gap fill 1-3, missing 2" ) {
+        sess->receive( fix::parse( "8=P|9=??|35=A|34=4|49=S|56=T|10=??|" ) );
+        REQUIRE( sender->sent( "8=P|9=??|35=A|34=1|49=T|56=S|10=??|" ) );
+        REQUIRE( sender->sent( "8=P|9=??|35=2|34=2|49=T|56=S|7=1|16=3|10=??|" ) );
+        REQUIRE( appl->is_logged_on() );
+        sess->receive( fix::parse( "8=P|9=??|35=A|34=1|49=S|56=T|10=??|" ) );
+        sess->receive( fix::parse( "8=P|9=??|35=D|34=3|49=S|56=T|10=??|" ) );
+        REQUIRE( !appl->is_logged_on() );
+        REQUIRE( !sess->is_connected() );
+    }
+
+    SECTION( "successful logon, gap fill 1-3, missing 5" ) {
+        sess->receive( fix::parse( "8=P|9=??|35=A|34=4|49=S|56=T|10=??|" ) );
+        REQUIRE( sender->sent( "8=P|9=??|35=A|34=1|49=T|56=S|10=??|" ) );
+        REQUIRE( sender->sent( "8=P|9=??|35=2|34=2|49=T|56=S|7=1|16=3|10=??|" ) );
+        REQUIRE( appl->is_logged_on() );
+        sess->receive( fix::parse( "8=P|9=??|35=D|34=36|49=S|56=T|10=??|" ) );
+        REQUIRE( !appl->is_logged_on() );
+        REQUIRE( !sess->is_connected() );
+    }
+    */
 }
